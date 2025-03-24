4bbf4a7e1a759d742685a23666c509bbec49cf17
jhsong <jh@song>
2025-03-24 23:11:03 +0900

    footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 import React from "react";
 import HomeHeader from "./components/HomeHeader";
-
+import Image from "next/image";
+import HomeSearch from "./components/HomeSearch";
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png"
+          width={300}
+          height={100}
+          priority
+          style={{ width: "auto" }}
+          alt="google logo"
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }

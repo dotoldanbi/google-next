3134d8727361a431549d994b718565f2271fd21b
jhsong <jh@song>
2025-03-25 16:32:27 +0900

    search header options

--- src/app/search/layout.jsx
+++ src/app/search/layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchHeader from "../components/SearchHeader";
 import "./../globals.css";
+import SearchHeaderOptions from "../components/SearchHeaderOptions";
 
 export default function layout({ children }) {
   return (
     <div>
       <SearchHeader />
+
       {children}
     </div>
   );

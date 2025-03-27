e224b34902e75eb9a08e4c279605dc75fde7ed2a
jhsong <jh@song>
2025-03-27 11:53:08 +0900

    useSearchParams

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { BsFillMicFill } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
@@ -17,25 +17,27 @@ export default function SearchBox() {
 
   const [term, setTerm] = useState(searchTerm || "");
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
-    >
-      <input
-        type="text"
-        className="w-full focus:outline-none"
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-      />
-      <RxCross2
-        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
-        onClick={() => setTerm("")}
-      />
-      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 border-l-2 border-gray-300 mr-3 pl-4" />
-      <AiOutlineSearch
-        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
-        onClick={handleSubmit}
-      />
-    </form>
+    <Suspense>
+      <form
+        onSubmit={handleSubmit}
+        className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+      >
+        <input
+          type="text"
+          className="w-full focus:outline-none"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <RxCross2
+          className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+          onClick={() => setTerm("")}
+        />
+        <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 border-l-2 border-gray-300 mr-3 pl-4" />
+        <AiOutlineSearch
+          className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+          onClick={handleSubmit}
+        />
+      </form>
+    </Suspense>
   );
 }

16ee225aa6fc4d77f5e2137498a8f033a387f6b4
jhsong <jh@song>
2025-03-27 11:55:19 +0900

    useSearchParams

--- src/app/components/PaginationButtons.jsx
+++ src/app/components/PaginationButtons.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 
 import { usePathname, useSearchParams } from "next/navigation";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
@@ -11,31 +11,35 @@ export default function PaginationButtons() {
   const startIndex = searchParams.get("start") || "1";
 
   return (
-    <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
-      {startIndex >= 10 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
-        >
-          <div className="flex items-center cursor-pointer hover:underline">
-            <BsChevronLeft className="h-5" />
-            <p>Previous</p>
-          </div>
-        </Link>
-      )}
+    <Suspense>
+      <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
+        {startIndex >= 10 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              startIndex - 10
+            }`}
+          >
+            <div className="flex items-center cursor-pointer hover:underline">
+              <BsChevronLeft className="h-5" />
+              <p>Previous</p>
+            </div>
+          </Link>
+        )}
 
-      {startIndex <= 90 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${
-            Number(startIndex) + 10
-          }`}
-        >
-          <div className="flex items-center cursor-pointer hover:underline">
-            <BsChevronRight className="h-5" />
-            <p>Next</p>
-          </div>
-        </Link>
-      )}
-    </div>
+        {startIndex <= 90 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              Number(startIndex) + 10
+            }`}
+          >
+            <div className="flex items-center cursor-pointer hover:underline">
+              <BsChevronRight className="h-5" />
+              <p>Next</p>
+            </div>
+          </Link>
+        )}
+      </div>
+    </Suspense>
   );
 
   return <div>PaginationButtons</div>;
